85355fa3d563e60dfc74721e6a08ab4102803012
Merve Erdol <merve-uygun@windowslive.com>
2017-12-11 22:34:21 +0300

    Put video to canvas

--- scripts.js
+++ scripts.js
@@ -17,4 +17,16 @@ function getVideo() {
         console.error(`OH NO`, err);
     })
 }
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
 getVideo();
