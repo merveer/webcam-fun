c06601024d717496dc316493792e8bf629cb209b
Merve Erdol <merve-uygun@windowslive.com>
2017-12-12 10:55:58 +0300

    Add rgb split effect to video

--- scripts.js
+++ scripts.js
@@ -27,7 +27,8 @@ function paintToCanvas() {
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        //pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -52,5 +53,14 @@ function redEffect(pixels) {
     }
     return pixels;
     }
+
+    function rgbSplit(pixels) {
+        for(let i = 0; i < pixels.data.length; i+=4){
+            pixels.data[i - 150] = pixels.data[i + 0];
+            pixels.data[i + 500] = pixels.data[i + 1];
+            pixels.data[i - 150] = pixels.data[i + 2];
+        }
+        return pixels;
+        }
 
     getVideo();
